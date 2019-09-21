7b0555dc7fb7fabb355dff9a4ca492352a1ae310
Rafał Piekara <rafal@borsuquesquad.pl>
2019-09-21 17:41:39 +0200

    Add basic components structure to cra.

---
+++ src/templates/create-react-app/src/components/index.js
@@ -0,0 +1,3 @@
+import { Button, Input } from "./ui";
+
+export { Button, Input };
--- src/templates/create-react-app/src/index.js
+++ src/templates/create-react-app/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import { Button, Input } from "components";
 
 function App() {
   return (
     <main>
       <h1>Starter React Project</h1>
+      <Button />
+      <Input />
     </main>
   );
 }
