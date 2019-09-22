84b1572c281e31843a871039e0c981d02d6ac3a7
Rafał Piekara <rafal@borsuquesquad.pl>
2019-09-22 12:52:28 +0200

    Add some dependencies and devDependencies to rni.

--- src/projectTypes.js
+++ src/projectTypes.js
@@ -1,42 +1,51 @@
 module.exports = {
   npmProject: {
-    name: "npm-project",
+    name: 'npm-project',
     dependencies: [],
     devDependencies: []
   },
   createReactApp: {
-    name: "create-react-app",
+    name: 'create-react-app',
     dependencies: [
-      "axios",
-      "dotenv",
-      "history",
-      "i18next",
-      "keymirror",
-      "localforage",
-      "node-sass",
-      "react-app-rewire-styled-components",
-      "react-app-rewired",
-      "react-redux",
-      "redux",
-      "redux-persist",
-      "redux-thunk",
-      "styled-components",
-      "uuid",
-      "react-router-dom"
+      'axios',
+      'dotenv',
+      'history',
+      'i18next',
+      'keymirror',
+      'localforage',
+      'node-sass',
+      'react-app-rewire-styled-components',
+      'react-app-rewired',
+      'react-redux',
+      'redux',
+      'redux-persist',
+      'redux-thunk',
+      'styled-components',
+      'uuid',
+      'react-router-dom'
     ],
     devDependencies: [
-      "babel-plugin-styled-components",
-      "redux-devtools-extension",
-      "redux-logger"
+      'babel-plugin-styled-components',
+      'redux-devtools-extension',
+      'redux-logger'
     ]
   },
   reactNativeInit: {
-    name: "react-native-init",
-    dependencies: [],
-    devDependencies: []
+    name: 'react-native-init',
+    dependencies: [
+      'keymirror',
+      'react-native-gesture-handler',
+      'react-native-vector-icons',
+      'react-navigation',
+      'redux',
+      'react-redux',
+      'redux-persist',
+      'redux-thunk'
+    ],
+    devDependencies: ['redux-logger', 'redux-devtools-extension']
   },
   vue: {
-    name: "vue",
+    name: 'vue',
     dependencies: [],
     devDependencies: []
   }
